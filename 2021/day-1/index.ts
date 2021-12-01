7a40e0e67bb391d58e13a35bef513f9fddf09f2a
JeanBaptisteScanavino <jbscana@gmail.com>
2021-12-01 21:27:05 +0100

    Day 1 OK

--- 2021/day-1/index.ts
+++ 2021/day-1/index.ts
@@ -1,9 +1,19 @@
-import { readFileSync } from 'fs';
+export const readListAndNumberify = (input) => input.split('\n').map((e)=> parseInt(e))
+    
+export const decreasedMesure = (part, input) => {
+    let result = 0;
+    for(let i=0; i< input.length; i++){
+        if (input[i+1]> input[i]){
+            result++;
+        }
+    }
+    console.info(`${part}: ${result}`)
+}
 
-export const readFile = (): string => readFileSync('input.txt', { encoding: 'utf8' }).trim();
-
-const main = (): void => {
-    console.info(readFile());
-};
-
-main();
+export const makeWindowsByThree = (input) =>{
+    let windows =[]
+    for(let i=0; i< input.length-2; i++){
+        windows.push(input[i]+input[i+1]+input[i+2])
+    }
+    return windows;
+}
--- 2021/index.ts
+++ 2021/index.ts
@@ -1,9 +1,14 @@
+import { decreasedMesure, makeWindowsByThree, readListAndNumberify } from './day-1';
 import { readFileSync } from 'fs';
 
 export const readFile = (): string => readFileSync('input.txt', { encoding: 'utf8' }).trim();
 
 const main = (): void => {
-    console.info(readFile());
+    const inputData= readFile()
+    const cleanData = readListAndNumberify(inputData)
+    decreasedMesure('Part 1', cleanData)
+    const windows = makeWindowsByThree(cleanData)
+    decreasedMesure('Part 2', windows)
 };
 
 main();
