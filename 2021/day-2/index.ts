f04a9db012450c268d126616b962eaa5e233e88d
JeanBaptisteScanavino <jbscana@gmail.com>
2021-12-02 20:49:44 +0100

    Day 2 Ok

--- 2021/day-2/index.ts
+++ 2021/day-2/index.ts
@@ -23,9 +23,38 @@ const readMouvement = (input: string[][]): number => {
 
     return horizontalPosition * depth;
 };
+
+export const readMouvementWithAim = (input: string[][]): number => {
+    let horizontalPosition = 0;
+    let depth = 0;
+    let aim = 0;
+    input.forEach((element) => {
+        switch (element[0]) {
+            case 'forward':
+                horizontalPosition += parseInt(element[1], 10);
+                depth += parseInt(element[1], 10) * aim;
+                break;
+            case 'down':
+                aim += parseInt(element[1], 10);
+                break;
+            case 'up':
+                aim -= parseInt(element[1], 10);
+                break;
+            default:
+                // eslint-disable-next-line no-console
+                console.log('error Default statement');
+                break;
+            }
+        });
+
+    return horizontalPosition * depth;
+};
+
 export const dayTwo = (inputData: string): void => {
     const dataList = readListAndShapefy(inputData);
     const result = readMouvement(dataList);
     // eslint-disable-next-line no-console
-    console.log(result);
+    console.log(`Part One: ${result}`);
+    const resultTwo= readMouvementWithAim(dataList);
+    console.log(`Part two: ${resultTwo}`)
 };
